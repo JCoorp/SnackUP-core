3a6964f43fdcfa97b6f82db420cc508977f8be6a
JCoorp <carlosjm.ti23@utsjr.edu.mx>
2026-09-25 01:12:07 -0600

    canva-bridge: add index.tsx

---
+++ tools/canva-bridge/canva-editor/index.tsx
@@ -0,0 +1,15 @@
+import { AppUiProvider } from "@canva/app-ui-kit";
+import "@canva/app-ui-kit/styles.css";
+import { prepareDesignEditor, type DesignEditorIntent } from "@canva/intents/design";
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
+
+async function render() {
+  const root = createRoot(document.getElementById("root") as Element);
+  root.render(<AppUiProvider><App /></AppUiProvider>);
+}
+
+const designEditor: DesignEditorIntent = { render };
+prepareDesignEditor(designEditor);
+
+if (module.hot) module.hot.accept("./app", render);
